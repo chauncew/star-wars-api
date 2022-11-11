579f9947865fea5d612dce0141bd07dd7379d7ea
Chaunce W <chauncew1137@gmail.com>
2022-11-11 08:37:46 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const getData = async () => {
+//     try{
+//     const fetchData = await fetch("https://akabab.github.io/starwars-api/api/all.json")
+//     const data = await fetchData.json()
+//     console.log(data)
+//     data.map((info) => {
+//         testing.innerHTML = `<img src=${info.image}></img>`
+//     })
+//     } catch(error) {
+//         console.log(`Error fetching data. ${error}`)
+//     }
+// }
+
+// getData()
+
+// const testing = document.querySelector('.testing')
+
