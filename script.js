096ce399d641140ea56063bc62350d0ffe81e6b3
Chaunce W <chauncew1137@gmail.com>
2023-07-29 10:03:53 -0500

    Putting finishing touches

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const starCards = document.querySelector(".star-cards ")
-const testing = document.querySelector(".testing")
-const close = document.querySelector('.close')
-const closeTab = document.querySelector(".closeTab")
-const body = document.querySelector('body')
-const wiki = document.querySelector('.star-cards a')
+const starCards = document.querySelector(".star-cards ");
+const testing = document.querySelector(".testing");
+const close = document.querySelector('.close');
+const closeTab = document.querySelector(".closeTab");
+const body = document.querySelector('body');
+const wiki = document.querySelector('.star-cards a');
 
 
 
@@ -27,30 +27,30 @@ const getData = async () => {
             </div>
         </div>
         `
-    })
+    });
     } catch(error) {
         console.log(`Error fetching data. ${error}`)
-    }
-}
-getData()
+    };
+};
+getData();
 
 //Quotes Section Animations
-gsap.registerPlugin()
+gsap.registerPlugin();
 
-const quotes1 = gsap.timeline()
+const quotes1 = gsap.timeline();
 quotes1
 .from('.frame1', {autoAlpha: 0, x: -800, duration: 0.5})
-const quotes2 = gsap.timeline()
+const quotes2 = gsap.timeline();
 .from('.frame2', {autoAlpha: 0, x: 800, duration: 0.5})
-const quotes3 = gsap.timeline()
+const quotes3 = gsap.timeline();
 .from('.frame3', {autoAlpha: 0, x: -800, duration: 0.5})
-const quotes4 = gsap.timeline()
+const quotes4 = gsap.timeline();
 .from('.frame4', {autoAlpha: 0, x: 800, duration: 0.5})
-const quotes5 = gsap.timeline()
+const quotes5 = gsap.timeline();
 .from('.frame5', {autoAlpha: 0, x: -800, duration: 0.5})
-const quotes6 = gsap.timeline()
+const quotes6 = gsap.timeline();
 .from('.frame6', {autoAlpha: 0, x: 800, duration: 0.5})
-const quotes7 = gsap.timeline()
+const quotes7 = gsap.timeline();
 .from('.vader-last-word', {autoAlpha: 0, opacity: 0, duration: 0.5})
 
 ScrollTrigger.create({
@@ -58,40 +58,40 @@ ScrollTrigger.create({
     trigger: '.quotes-heading',
     start: 'top 35%',
     end: '100%',
-})
+});
 ScrollTrigger.create({
     animation: quotes2,
     trigger: '.frame2',
     start: 'top 85%',
     end: '100%',
-})
+});
 ScrollTrigger.create({
     animation: quotes3,
     trigger: '.frame3',
     start: 'top 85%',
     end: '100%',
-})
+});
 ScrollTrigger.create({
     animation: quotes4,
     trigger: '.frame4',
     start: 'top 85%',
     end: '100%',
-})
+});
 ScrollTrigger.create({
     animation: quotes5,
     trigger: '.frame5',
     start: 'top 85%',
     end: '100%',
-})
+});
 ScrollTrigger.create({
     animation: quotes6,
     trigger: '.frame6',
     start: 'top 85%',
     end: '100%',
-})
+});
 ScrollTrigger.create({
     animation: quotes7,
     trigger: '.vader-last-word',
     start: 'top 85%',
     end: '100%',
-})
+});
